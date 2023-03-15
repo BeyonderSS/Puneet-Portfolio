c6f62910220380816190f39f2661d806c92a0dcf
BeyonderSS <ssbeyonder@gmail.com>
2023-03-15 20:34:56 +0530

    main

--- components/About.tsx
+++ components/About.tsx
@@ -17,7 +17,7 @@ function About({}: Props) {
         opacity: 1,
       }}
     className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute  top-10 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
       <motion.img
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { motion } from "framer-motion";
-
-type Props = {
-  directionLeft?: boolean;
-};
+type Props = { directionLeft?: boolean };
 
 const Skill = (directionLeft: Props) => {
   return (
-    <div className=" group relative flex cursor-pointer">
+    <div className="group relative flex cursor-pointer">
       <motion.img
         initial={{
           x: directionLeft ? -200 : 200,
@@ -18,12 +15,16 @@ const Skill = (directionLeft: Props) => {
         }}
         whileInView={{
           opacity: 1,
-          y: 0,
+          x: 0,
         }}
-        src="https://www.vhv.rs/dpng/d/456-4562295_library-of-javascript-icon-graphic-freeuse-png-files.png"
-        className="rounded-full border border-gray-500 object-cover w-20 h-20 xl:w-25 xl:h-25 filter"
-
-      />
+        src="https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_96102ac6497377cd53da621075fe828e/sanity.png"
+        className="rounded-full border border-gray-400 object-cover w-20 h-20 xl:h-25 xl:w-25 filter group-hover:grayscale transition duration-300 ease-in-out"
+      /> 
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-20 h-20 xl:h-25 xl:w-25 rounded-full z-0">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+        </div>
+      </div>
     </div>
   );
 };
